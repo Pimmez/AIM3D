5f4df5ce2bd0823d4617ab8a7198a156771060b8
Daniel <idaniel95@me.com>
2015-12-08 21:39:47 +0100

    Started with checkifinview script, and made camerafogsettings.
    checkifinview checks if the object that that script is on is in the
    camera view, we will use this so that we know which interactive objects
    can be activated (in your view).
    
    camerafogsettings allows us the change if fog is enabled/ or change the
    fog density. this way we can give one camera a better sight than the
    other.

---
+++ Assets/Scripts/Players/Movement/MouseLook.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour {
+
+    [SerializeField]
+    private float lookSensitivity = 5;
+
+    [SerializeField]
+    private float lookSmoothDamp = 0.1f;
+
+    private float xRotation;
+
+    private float yRotation;
+
+    private float currentXRotation;
+
+    private float currentYRotation;
+
+    private float xRotationVelocity;
+
+    private float yRotationVelocity;
+
+    private Transform character;
+
+    // Use this for initialization
+    void Awake () {
+        character = transform.parent;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        yRotation += Input.GetAxis("Mouse X") * lookSensitivity;
+        xRotation -= Input.GetAxis("Mouse Y") * lookSensitivity;
+
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        currentXRotation = Mathf.SmoothDamp(currentXRotation, xRotation, ref xRotationVelocity, lookSmoothDamp);
+        currentYRotation = Mathf.SmoothDamp(currentYRotation, yRotation, ref yRotationVelocity, lookSmoothDamp);
+
+        character.transform.rotation = Quaternion.Euler(0, currentYRotation, 0);
+        transform.rotation = Quaternion.Euler(currentXRotation, currentYRotation, 0);
+	}
+}
